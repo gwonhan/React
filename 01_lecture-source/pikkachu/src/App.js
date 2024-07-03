42438be4cd80c8206aac354c6bf9975326ac1873
DESKTOP-0CUIJL6\LEE <h2odream@naver.com>
2024-07-03 15:40:17 +0900

    01

---
+++ 01_lecture-source/pikkachu/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
+
+function App() { 
+
+  const [image, setimage] = useState(null);
+  const [Text, setText] = useState('');
+
+
+
+  const onClickHandler =()=>{
+    
+    axios.get(`https://pokeapi.co/api/v2/pokemon-form/${Text}`)
+    .then(res =>setimage(res.data.sprites.front_shiny))
+    .catch(err=>console.log(err))
+    
+  }
+
+  return (
+  <>
+  <label>피카츄 검색하기</label>
+  <input type="text" onChange={ e=> setText(e.target.value) }/>
+  <button onClick={ onClickHandler }> 검색 </button>
+  {image ? <img src={image} alt="pk"/> : <div>검색해</div>}
+  
+  </>
+  
+  );
+}
+
+export default App;
